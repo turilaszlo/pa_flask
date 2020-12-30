440279aae1f57795852975572b271eff64e25184
turi <turi66@gmail.com>
2020-12-30 19:22:11 +0000

    food elso verzio

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from foods import foodSelector
 
 app = Flask(__name__,
             static_url_path='',
@@ -6,8 +7,28 @@ app = Flask(__name__,
             template_folder='templates/')
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+@app.route('/food')
+def food():
+    itemNo=5
+    calMax=1000
+    cvitMin=50
+    requestedCvit=3000
+
+    foodDict=foodSelector(itemNo,calMax,cvitMin)
+
+    runX=0
+    foodDict=foodSelector(itemNo,calMax,cvitMin)
+
+    while True:
+        foodDict.get("totCvits")<requestedCvit
+        runX=runX+1
+        foodDict=foodSelector(itemNo,calMax,cvitMin)
+        #print("runX: " + str(runX)+ "// totalCvit: " + str(foodDict.get("totCvits")))
+        if foodDict.get("totCvits")>=(requestedCvit):
+            return render_template('food.html',foodDict=foodDict,itemNo=itemNo,runX=str(runX))
+
 
 app.config["DEBUG"] = True
-

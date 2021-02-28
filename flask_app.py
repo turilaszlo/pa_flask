a240d7624e846814a8168f583e6ec8c62773163c
turi <turi66@gmail.com>
2021-02-28 13:12:24 +0000

    vegie finder 1st rel

--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from foods import foodSelector
+from fdc import fdcSelector
 
 app = Flask(__name__,
             static_url_path='',
             static_folder='bootstrap/',
             template_folder='templates/')
 
-@app.route('/')
+
+
+@app.route('/lturi')
 def home():
     return render_template('home.html')
+
+@app.route('/')
+def ideateka():
+    return render_template('ideateka.html')
 
 @app.route('/food')
 def food():
@@ -30,5 +37,20 @@ def food():
         if foodDict.get("totCvits")>=(requestedCvit):
             return render_template('food.html',foodDict=foodDict,itemNo=itemNo,runX=str(runX))
 
+@app.route('/fdc/<int:itemNo>/<int:Kcal>/<int:cvit>/<minmaxKcal>/<minmaxCvit>')
+def fdc(itemNo,Kcal,cvit,minmaxKcal,minmaxCvit):
+    return render_template('fdc.html',fdcDict=fdcSelector(Kcal,itemNo,cvit,minmaxKcal,minmaxCvit),itemNo=itemNo,Kcal=Kcal,cvit=cvit,minmaxKcal=minmaxKcal,minmaxCvit=minmaxCvit)
+
+@app.route('/fdc_in',methods=['GET','POST'])
+def fdc_in():
+    if request.method == 'POST':
+        item=request.form['itemInput']
+        Kcal=request.form['KcalInput']
+        minmaxKcal=request.form['minmaxKcalInput']
+        cvit=request.form['cvitInput']
+        minmaxCvit=request.form['minmaxCvitInput']
+        return redirect(url_for('fdc',Kcal=Kcal,itemNo=item,cvit=cvit,minmaxKcal=minmaxKcal,minmaxCvit=minmaxCvit))
+
+    return render_template('fdc_in.html')
 
 app.config["DEBUG"] = True

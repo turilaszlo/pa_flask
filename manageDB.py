5f9bb95f9d4ba4a1ad87f513ab69442e1f34584c
turilaszlo <46007001+turilaszlo@users.noreply.github.com>
2021-02-28 14:17:35 +0100

    Delete manageDB.py

--- manageDB.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-
-def foodDBopen():
-    cnx=mysql.connector.connect(host='turi.mysql.pythonanywhere-services.com',
-                                user='turi',
-                                password='xxxxxxxxxx',
-                                database='turi$food')
-    return cnx
